e0fa3b08a949996f73f785002ff7a423fbc41d89
David Nolen <david.nolen@gmail.com>
2011-04-12 11:51:53 -0400

    * .gitignore: initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject match "0.1.0-SNAPSHOT"
+  :description "Optimized pattern matching and predicate dispatch for Clojure"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ test/match/test/core.clj
@@ -0,0 +1,6 @@
+(ns match.test.core
+  (:use [match.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
