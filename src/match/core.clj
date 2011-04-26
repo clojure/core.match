8ace8a9a85fc4715827c28f2e29e38664fcee699
David Nolen <david.nolen@gmail.com>
2011-04-25 20:28:15 -0400

    * src/match/core.clj: tabled implies goal

--- src/match/core.clj
+++ src/match/core.clj
@@ -1,10 +1,19 @@
 (ns match.core
   (:refer-clojure :exclude [reify == inc])
-  (:use [logos minikanren rel]))
+  (:use [logos minikanren tabled rel]))
 
 (def method-table (atom {}))
 (def pred-table (atom {}))
+
 (defrel is a b)
+
+(def implies
+     (tabled [a b]
+       (conde
+         ((is a b))
+         ((exist [z]
+            (is a z)
+            (implies z b))))))
 
 (defn var->sym [v]
   (symbol (str (.ns v)) (name (.sym v))))
@@ -26,4 +35,12 @@
 
 (comment
   (defpred even? even?)
+
+  ;; could fix this with a macro
+  ;; are there composability expectations?
+  (fact is `even? `integer?)
+  (fact is `integer? `number?)
+
+  (run* [q]
+    (implies `even? q))
   )
