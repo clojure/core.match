f2eb584b9435e73f849fac76d3c9eabcff1402ef
David Nolen <david.nolen@gmail.com>
2011-05-24 00:32:08 -0400

    * src/match/scratch.clj: be clearer about activity

--- src/match/scratch.clj
+++ src/match/scratch.clj
@@ -5,9 +5,6 @@
   (:use [clojure.pprint :only [pprint]])
   (:import [java.io Writer]))
 
-(def guard-priorities {'= 0
-                       'isa? 1})
-
 (defn vec-drop-nth [v idx]
   (into (subvec v 0 idx)
         (subvec v (clojure.core/inc idx) (count v))))
@@ -45,6 +42,15 @@
      (Pattern. p gs)))
 
 (def ^Pattern wildcard (pattern '_))
+
+(defn wildcard? [p]
+  (identical? p wildcard))
+
+(defn constructor? [p]
+  (not (wildcard? p)))
+
+(declare useful-p?)
+(declare useful?)
 
 (defprotocol IPatternMatrix
   (width [this])
@@ -119,12 +125,6 @@
 (defn score-p [pm i j]
   )
 
-(defn wildcard? [p]
-  (identical? p wildcard))
-
-(defn constructor? [p]
-  (not (wildcard? p)))
-
 (defn necessary? [column]
   (every? (fn [p]
             (or (literal? p)
@@ -142,48 +142,10 @@
   (some #(useful-p? pm % j)
         (range (count (row pm j)))))
 
-(defn sort-guards [[as] [bs]]
-  (let [asi (get guard-priorities as 2)
-        bsi (get guard-priorities bs 2)]
-    (cond
-     (< asi bsi) -1
-     (> asi bsi) 1
-     :else 0)))
-
-(defn guards-for [p gs]
-  (sort sort-guards
-        (reduce (fn [s g]
-                  (if (contains? (set g) p)
-                    (conj s g)
-                    s))
-                [] gs)))
-
-(defn proc-row [[ps gs :as row]]
-  (vec
-   (map (fn [p]
-          (let [pgs (guards-for p gs)]
-            (pattern p pgs)))
-        ps)))
-
-(defn ms->pm [ms]
-  (pattern-matrix (map proc-row ms)))
+;; =============================================================================
+;; Active Work
 
 (comment
-  ;; create a pattern matrix
-  (def pm1 (pattern-matrix [[(pattern 'a '[(isa? B a)]) (pattern 0)]
-                           [(pattern 'a '[(isa? C a)]) (pattern 1)]]))
-
-  ;; raw signatures and guards to pattern matrix
-  (seq (ms->pm '[[[a b 0] [(isa? A a) (isa? B b)]]
-                 [[a b 1] [(isa? A a) (isa? B b)]]]))
-
-  ;; test the can look at the pm as a seq
-  (seq pm1)
-
-  (type pm1)
-  
-  (seq (swap pm1 1))
-
   ;; we're working with this at the moment, no guards, no implication
   ;; just the basic Maranget algorithm. We'd like to be execute the following
   ;;
@@ -210,4 +172,54 @@
 
   (specialize (select pm2) (pattern true))
   (specialize (select pm2) (pattern false))
+  )
+
+;; =============================================================================
+;; On Hold
+
+(comment
+  (def guard-priorities {'= 0
+                       'isa? 1})
+
+  (defn sort-guards [[as] [bs]]
+    (let [asi (get guard-priorities as 2)
+          bsi (get guard-priorities bs 2)]
+      (cond
+       (< asi bsi) -1
+       (> asi bsi) 1
+       :else 0)))
+
+  (defn guards-for [p gs]
+    (sort sort-guards
+          (reduce (fn [s g]
+                    (if (contains? (set g) p)
+                      (conj s g)
+                      s))
+                  [] gs)))
+
+  (defn proc-row [[ps gs :as row]]
+    (vec
+     (map (fn [p]
+            (let [pgs (guards-for p gs)]
+              (pattern p pgs)))
+          ps)))
+
+  (defn ms->pm [ms]
+    (pattern-matrix (map proc-row ms)))
+  
+  ;; create a pattern matrix
+  (def pm1 (pattern-matrix [[(pattern 'a '[(isa? B a)]) (pattern 0)]
+                           [(pattern 'a '[(isa? C a)]) (pattern 1)]]))
+
+  ;; raw signatures and guards to pattern matrix
+  (seq (ms->pm '[[[a b 0] [(isa? A a) (isa? B b)]]
+                 [[a b 1] [(isa? A a) (isa? B b)]]]))
+
+  ;; test the can look at the pm as a seq
+  (seq pm1)
+
+  (type pm1)
+  
+  (seq (swap pm1 1))
+
   )
