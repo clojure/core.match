d36bf5e2232df9e33cabcde928e8a7a9648d073d
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-07-22 19:40:33 +0800

    Alpha version of match macro

--- test/match/test/core.clj
+++ test/match/test/core.clj
@@ -1,6 +1,27 @@
 (ns match.test.core
+  (:refer-clojure :exclude [reify == inc compile])
   (:use [match.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest simple-compile
+  (is
+    (= (compile (pattern-matrix [] '[]))
+       (fail-node)))
+  (is
+    (= (compile (pattern-matrix [] '[]))
+       (fail-node))))
+
+(def pm2 (pattern-matrix [(pattern-row [wildcard (pattern false) (pattern true)] :a1)
+                          (pattern-row [(pattern false) (pattern true) wildcard] :a2)
+                          (pattern-row [wildcard wildcard (pattern false)] :a3)
+                          (pattern-row [wildcard wildcard (pattern true)] :a4)]
+                         '[x y z]))
+
+;(emit-matrix 
+;  '[x y]
+;  [[_ true] 1
+;   [_ false] 2
+;   [true _] 3
+;   [true _] 4
+;   ]
+;  )
