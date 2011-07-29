abfd95268b57ed8bee7c6dcdc92769237abf3acc
David Nolen <david.nolen@gmail.com>
2011-07-29 19:12:37 -0400

    * project.clj: fix project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject match "0.1.0-SNAPSHOT"
   :description "Optimized pattern matching and predicate dispatch for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                 [core.logic "0.6.3"]]
+                 [org.clojure/core.logic "0.6.3"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
