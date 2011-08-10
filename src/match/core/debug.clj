d9b6517ff337cd2e8a0759fee259c9bb69a433cc
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-08-10 20:57:29 +0800

    Renamed debug macros to prevent conflicts with match.core

--- src/match/core/debug.clj
+++ src/match/core/debug.clj
@@ -3,16 +3,16 @@
   (:use [match.core :only [build-matrix compile to-clj source-pprint]])
   (:use [clojure.pprint :only [pprint]]))
 
-(defmacro p-to-matrix [vars & clauses]
+(defmacro m-to-matrix [vars & clauses]
   `(-> (build-matrix ~vars ~@clauses)
      pprint))
 
-(defmacro p-to-dag [vars & clauses]
+(defmacro m-to-dag [vars & clauses]
   `(-> (build-matrix ~vars ~@clauses)
      compile
      pprint))
 
-(defmacro p-to-clj [vars & clauses]
+(defmacro m-to-clj [vars & clauses]
   `(-> (build-matrix ~vars ~@clauses)
      compile
      to-clj
