64e87095cbbfc08c5aefe8fabeb8961e2fb0270f
David Nolen <david.nolen@gmail.com>
2011-08-11 08:31:53 -0400

    * src/match/core.clj: remove print-matrix from core ns, it's now in debug ns

--- src/match/core.clj
+++ src/match/core.clj
@@ -493,24 +493,6 @@
   (some #(useful-p? pm % j)
         (range (count (row pm j)))))
 
-(defn print-matrix
-  ([pm] (print-matrix pm 4))
-  ([pm col-width]
-     (binding [*out* (pp/get-pretty-writer *out*)]
-       (print "|")
-       (doseq [o (occurrences pm)]
-         (pp/cl-format true "~4D~7,vT" o col-width))
-       (print "|")
-       (prn)
-       (doseq [[i row] (map-indexed (fn [p i] [p i]) (rows pm))]
-         (print "|")
-         (doseq [p (patterns row)]
-           (pp/cl-format true "~4D~7,vT" (str p) col-width))
-         (print "|")
-         (print " " (action-for-row pm i))
-         (prn))
-       (println))))
-
 ;; =============================================================================
 ;; Default Matrix Specialization
 
