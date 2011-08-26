59a23be1847e1d3662896cb737150743a56e94bd
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-08-26 16:33:00 +0800

    Added Java Bean pattern matching example for Date

--- src/match/date.clj
+++ src/match/date.clj
@@ -11,3 +11,11 @@
       :hours   (.getHours this)
       :minutes (.getMinutes this)
       not-found)))
+
+
+;; Example implementation using clojure.core/bean
+(comment
+  (extend-type java.util.Date
+    IMatchLookup
+    (val-at* [this k not-found]
+      (get (bean this) k not-found))))
