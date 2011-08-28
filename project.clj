647fd676ac5f3f331c27d1fc2429fe08206d1887
David Nolen <david.nolen@gmail.com>
2011-08-28 18:00:40 -0400

    * project.clj: bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject match "0.1.0-SNAPSHOT"
+(defproject match "0.2.0-SNAPSHOT"
   :description "Optimized pattern matching and predicate dispatch for Clojure"
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [lein-clojars "0.7.0"]
