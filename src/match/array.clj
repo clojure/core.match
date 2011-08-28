1ef5f0fd3115aa03451981fa4a700cdf7dc08b58
David Nolen <david.nolen@gmail.com>
2011-08-28 16:02:11 -0400

    * src/match/array.clj: clean up comments

--- src/match/array.clj
+++ src/match/array.clj
@@ -33,29 +33,6 @@
   [_] ObjectArray)
 
 (comment
-  ;; hmm interesting, this is where type information would help
-  (do
-    (set! *warn-on-reflection* true)
-    (let [t (object-array [:black (object-array [:red (object-array [:red 1 2 3]) 3 4]) 5 6])]
-      (match [t]
-        [(([:black ([:red ([:red _ _ _] ::objects) _ _] ::objects) _ _] ::objects) |
-          ([:black ([:red _ _ ([:red _ _ _] ::objects)] ::objects) _ _] ::objects) |
-          ([:black _ _ ([:red ([:red _ _ _] ::objects) _ _] ::objects)] ::objects))] :valid
-          :else :invalid)))
-
-  ;; 220ms
-  (let [^objects t (object-array [:black (object-array [:red (object-array [:red 1 2 3]) 3 4]) 5 6])]
-   (dotimes [_ 10]
-     (time
-      (dotimes [_ 1e7]
-        (match [t]
-          [(([:black ([:red ([:red _ _ _] ::objects) _ _] ::objects) _ _] ::objects) |
-            ([:black ([:red _ _ ([:red _ _ _] ::objects)] ::objects) _ _] ::objects) |
-            ([:black _ _ ([:red ([:red _ _ _] ::objects) _ _] ::objects)] ::objects))] :valid
-            :else :invalid)))))
-  )
-
-(comment
   (let [x (int-array [1 2 3])]
     (match [x]
       [([_ _ 2] ::ints)] :a0
@@ -93,13 +70,14 @@
            [([1 1 3] ::ints :offset o)] :a1
            [([1 2 3] ::ints :offset o)] :a2)))))
 
-  ;; better syntax
-  (let [x (int-array [1 2 3])]
-    (dotimes [_ 10]
-      (time
-       (dotimes [_ 1e7]
-        (match [^{:vec ::ints :offset 0} x]
-          [[_ _ 2]] :a0
-          [[1 1 3]] :a1
-          [[1 2 3]] :a2)))))
+  ;; 220ms
+  (let [^objects t (object-array [:black (object-array [:red (object-array [:red 1 2 3]) 3 4]) 5 6])]
+   (dotimes [_ 10]
+     (time
+      (dotimes [_ 1e7]
+        (match [t]
+          [(([:black ([:red ([:red _ _ _] ::objects) _ _] ::objects) _ _] ::objects) |
+            ([:black ([:red _ _ ([:red _ _ _] ::objects)] ::objects) _ _] ::objects) |
+            ([:black _ _ ([:red ([:red _ _ _] ::objects) _ _] ::objects)] ::objects))] :valid
+            :else :invalid)))))
  )
