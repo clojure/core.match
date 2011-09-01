86128c9919d7f55a464b9b5028636dd59232ea83
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-09-01 16:09:32 +0800

    Changed Date pattern implementation to use bean-match

--- src/match/date.clj
+++ src/match/date.clj
@@ -1,21 +1,4 @@
 (ns match.date
-  (:use [match.core :only [IMatchLookup]]))
+  (:use [match.java :only [bean-match]]))
 
-(extend-type java.util.Date
-  IMatchLookup
-  (val-at* [this k not-found]
-    (case k
-      :year    (.getYear this)
-      :month   (.getMonth this)
-      :date    (.getDate this)
-      :hours   (.getHours this)
-      :minutes (.getMinutes this)
-      not-found)))
-
-
-;; Example implementation using clojure.core/bean
-(comment
-  (extend-type java.util.Date
-    IMatchLookup
-    (val-at* [this k not-found]
-      (get (bean this) k not-found))))
+(bean-match java.util.Date)
