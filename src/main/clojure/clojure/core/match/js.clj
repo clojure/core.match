18783b54aacccde2629a9a54ae8b706e63944cc0
David Nolen <david.nolen@gmail.com>
2011-10-02 17:33:15 -0400

    * src/main/clojure/clojure/core/match/js.clj: erg missing file * README.md: erg missing file

---
+++ src/main/clojure/clojure/core/match/js.clj
@@ -0,0 +1,11 @@
+(ns clojure.core.match.js
+  (:refer-clojure :exclude [compile])
+  (:use [clojure.core.match.core :exclude [match]]))
+
+(defmacro match 
+  [vars & clauses]
+  (binding [*clojurescript* true
+            *line* (-> &form meta :line)
+            *locals* (dissoc &env '_)
+            *warned* (atom false)]
+    `~(clj-form vars clauses)))
