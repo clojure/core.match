059b85ba5a0ae0c6269f71623ef57046a7bb8068
David Nolen <david.nolen@gmail.com>
2012-01-04 16:40:08 -0500

    * project.clj: remove clojure version dep

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject match "0.2.0-alpha8-SNAPSHOT"
   :description "Optimized pattern matching and predicate dispatch for Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
   :source-path "src/main/clojure"
   :test-path "src/test/clojure")
