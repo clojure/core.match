557768e22e699cb6a64b493c0a76d8a87870ec70
David Nolen <david.nolen@gmail.com>
2012-01-04 16:40:25 -0500

    * project.clj: update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject match "0.2.0-alpha8-SNAPSHOT"
+(defproject match "0.2.0-alpha10-SNAPSHOT"
   :description "Optimized pattern matching and predicate dispatch for Clojure"
   :source-path "src/main/clojure"
   :test-path "src/test/clojure")
