c56fb410cede56cb5aa39b24feca83b1be50d3b0
David Nolen <dnolen@Davids-MacBook-Pro-2.local>
2012-11-23 17:48:34 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
 (defproject match "0.2.0-alpha11-SNAPSHOT"
   :description "Optimized pattern matching and predicate dispatch for Clojure"
+
+  ;; lein 1
   :source-path "src/main/clojure"
-  :source-paths ["src/main/clojure"]
   :test-path "src/test/clojure"
+
+  ;; lein 2
+  :test-paths ["src/test/clojure"]
+  :source-paths ["src/main/clojure"]
+
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[lein-swank "1.4.4"]
-                     [lein-cljsbuild "0.2.5"]])
+                     [lein-cljsbuild "0.2.9"]])
