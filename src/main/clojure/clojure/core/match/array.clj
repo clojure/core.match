916e2b71cae5da886a0f7abbdbe5515ac84ca76b
David Nolen <david.nolen@gmail.com>
2013-06-19 16:42:34 -0400

    update array example

--- src/main/clojure/clojure/core/match/array.clj
+++ src/main/clojure/clojure/core/match/array.clj
@@ -80,29 +80,6 @@
            [([1 1 3] ::ints :offset o)] :a1
            [([1 2 3] ::ints :offset o)] :a2)))))
 
-  ;; this more complicated because we actually need to look at the values
-  (do
-    (set! *warn-on-reflection* true)
-
-    ;; 360ms
-    (let [^objects node (object-array [:black
-                          (object-array [:red
-                            (object-array [:red nil nil nil]) nil nil]) nil nil])]
-      (dotimes [_ 10]
-        (time
-         (dotimes [_ 1e5]
-           (balance-array node)))))
-
-    ;; 360ms
-    (let [^objects node (object-array [:black
-                            nil nil (object-array [:red nil nil
-                              (object-array [:red nil nil nil])])])]
-      (dotimes [_ 10]
-        (time
-         (dotimes [_ 1e5]
-           (balance-array node)))))
-    )
-
   (do
     (set! *warn-on-reflection* true)
     
@@ -122,74 +99,20 @@
     
     (defn balance-array [^objects node]
       (matchv ::objects [node]
-        [[:black [:red [:red a x b] y c] z d]] (R (B a x b) y (B c z d))
-        [[:black [:red a x [:red b y c]] z d]] (R (B a x b) y (B c z d))
-        [[:black a x [:red [:red b y c] z d]]] (R (B a x b) y (B c z d))
-        ))
-
-    ;; 14ms
-    ;; if we add the last case we get a 14X slow down, what gives?
-    ;; is this simply related to source code size
-    ;; last case we see 2528 LOC (poorly formatted) for 4 cases
-    ;; for 3 cases we see 1244 LOC for 3 cases
-
-    (let [^objects node (B (R (R nil nil nil) nil nil) nil nil)]
-      (dotimes [_ 10]
-        (time
-         (dotimes [_ 1e6]
-           (balance-array node)))))
-
-    ;; strange it looks like we actually see the inlining happen
-    #_(let [^objects node (B nil nil (R (R nil nil nil) nil nil))]
-      (dotimes [_ 10]
-        (time
-         (dotimes [_ 1e5]
-           (balance-array node)))))
-    )
-
-  (do
-    (set! *warn-on-reflection* true)
-    
-    (defmacro asets [a vs]
-      `(do
-         ~@(map (fn [a b c] (concat a (list b c)))
-                (repeat `(aset ~a)) (range (count vs)) vs)
-         ~a))
-    
-    (defn B ^objects [l v r]
-      (let [^objects o (make-array Object 4)]
-        (asets o [:black l v r])))
-
-    (defn R ^objects [l v r]
-      (let [^objects o (make-array Object 4)]
-        (asets o [:red l v r])))
-    
-    (defn balance-array [^objects node]
-      (matchv ::objects [node]
-         [([:black [:red [:red a x b] y c] z d] |
-           [:black [:red a x [:red b y c]] z d] |
-           [:black a x [:red [:red b y c] z d]] |
-           [:black a x [:red b y [:red c z d]]])] (R (B a x b) y (B c z d))
-         :else node))
+         [(:or [:black [:red [:red a x b] y c] z d]
+               [:black [:red a x [:red b y c]] z d]
+               [:black a x [:red [:red b y c] z d]]
+               [:black a x [:red b y [:red c z d]]])] :balance
+         :else :balanced))
 
     ;; 200ms
-    (let [^objects node (B (R (R nil nil nil) nil nil) nil nil)]
+    (let [^objects node (B nil nil (R nil nil (R nil nil nil)))]
       (dotimes [_ 10]
         (time
-         (dotimes [_ 1e5]
+         (dotimes [_ 1e7]
            (balance-array node)))))
+
+    #_(let [^objects node (B nil nil (R nil nil (B nil nil nil)))]
+      (balance-array node))
     )
-
-  (let [^objects node (object-array [:black
-                        nil nil (object-array [:red nil nil
-                          (object-array [:red nil nil nil])])])]
-    (balance-array node))
-
-  ;; 90ms
-  (dotimes [_ 10]
-    (time
-     (dotimes [_ 1e5]
-         (object-array [:black
-           (object-array [:red
-             (object-array [:red nil nil nil]) nil nil]) nil nil]))))
  )
